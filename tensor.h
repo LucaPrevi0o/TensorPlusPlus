53bc98602f32fc8a52135d94f4c0a6670d90ff49
LucaPrevi0o <luca01.previati@edu.unife.it>
2025-09-13 10:05:39 +0200

    added dot product (element-wise multiplication

--- tensor.h
+++ tensor.h
@@ -586,6 +586,25 @@ namespace tensor {
     using tuple = tensor<A, 1>;
 
     /**
+     * @brief Calculate the dot product (element-wise multiplication) of two matrices.
+     * 
+     * @tparam T Type of the matrix elements
+     * @param a First matrix
+     * @param b Second matrix
+     * @return Dot product of the two matrices
+     */
+    template<typename A>
+    matrix<A> dot(matrix<A> a, matrix<A> b) {
+
+        if (a.size()[1] != b.size()[0]) throw "Matrix dimensions do not match for dot product";
+
+        matrix<A> result(a.size()[0], b.size()[1]);
+        for (int i = 0; i < a.size()[0]; i++) for (int j = 0; j < b.size()[1]; j++)
+            for (int k = 0; k < a.size()[1]; k++) result(i, j) += a(i, k) * b(k, j);
+        return result;
+    }
+
+    /**
      * @brief Calculate the transpose of a matrix.
      * 
      * @tparam T Type of the matrix elements
